4bd4ff002d6fcaa718dc104500e7cae2dfedc6bc
Fnbellomo <fnbellomo@gmail.com>
2017-10-26 12:31:30 -0400

    more spam

--- static/js/map.js
+++ static/js/map.js
@@ -7,50 +7,90 @@ var origin_lon = -38
 var tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 var attribution = '<a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
 
-var innerHTML_empty = `
+var box_base = `
 <div class="info-box">
   <h4 class="info-title">Informacion de las Provincias en la que trabajamos</h4>
-  <div class="info-text">
+
+  <div class="info-text in">
     <br />Aca tiene que ir un texto explicando el trabajo que han realizado usteds.
     <br />Hace click sobre alguna
   </div><!--/info-text -->
 
-  <div id="info-graph">
-    <svg id="bar_plot"></svg>
-  </div><!--/info-graph -->
-
   <div id="box-imgs">
-  </div>
+  </div><!--/box-imgs -->
 
 </div><!--/info-box -->
 `;
 
-var imgs_html = `
-<div class="row justify-content-center box">
-  <div class="col-8">
-    <div id="lightgallery">
-      <a href="static/imgs/Misiones Habilidades Laborales4.jpg">
-          <img src="static/imgs/Misiones Habilidades Laborales4_thumb.jpg" />
-      </a>
-      <a href="static/imgs/Misiones Lib Expresion 2.jpg">
-          <img src="static/imgs/Misiones Lib Expresion 2_thumb.jpg" />
-      </a>
-    </div>
-  </div>
-</div>
+var html_media = `
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col">
+      <div id="lightgallery">
+          <a href="static/imgs/Misiones Habilidades Laborales4.jpg">
+            <img src="static/imgs/Misiones Habilidades Laborales4_thumb.jpg" class="img-thumbnail" style="max-width: 25%;" />
+          </a>
+
+          <a href="static/imgs/Misiones Lib Expresion 1.jpg">
+            <img src="static/imgs/Misiones Lib Expresion 1_thumb.jpg" class="img-thumbnail" style="max-width: 25%;" />
+          </a>
+
+          <a href="static/imgs/Misiones Lib Expresion 2.jpg">
+            <img src="static/imgs/Misiones Lib Expresion 2_thumb.jpg" class="img-thumbnail" style="max-width: 25%;" />
+          </a>
+
+          <a href="static/imgs/Misiones Taller TICs (1).jpg">
+            <img src="static/imgs/Misiones Taller TICs (1)_thumb.jpg" class="img-thumbnail" style="max-width: 25%;" />
+          </a>
+      </div><!--/lightgallery -->
+    </div><!--col -->
+  </div><!--row -->
+</div><!--/container -->
 `;
+
+var html_media = `
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-8">
+      <div id="lightgallery">
+        <div id="video-gallery">
+          <a href="https://vimeo.com/240002168" data-poster="static/imgs/santa_fe_1_t.png" >
+            <img src="static/imgs/santa_fe_1_t.png" class="img-thumbnail" style="max-width: 49%;" />
+          </a>
+
+          <a href="https://vimeo.com/239999840" data-poster="static/imgs/santa_fe_2_t.png">
+            <img src="static/imgs/santa_fe_2_t.png" class="img-thumbnail" style="max-width: 49%;" />
+          </a>
+        </div>
+        </div><!--/lightgallery -->
+    </div><!--col -->
+  </div><!--row -->
+</div><!--/container -->
+`
+
+var html_media = `
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-8">
+
+      <iframe src="https://player.vimeo.com/video/239996754?autoplay=1&byline=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+
+    </div><!--/col -->
+  </div><!--/row -->
+</div><!--/container -->
+`
 
 
 /*
   Styles
 */
 function getColor(d) {
-    return d == "Jujuy"        ? '#66c2a5' :
-           d == "Santa Fe"     ? '#fc8d62' :
-           d == "Misiones"     ? '#8da0cb' :
-           d == "Tucuman"      ? '#e78ac3' :
-           d == "Salta"        ? '#a6d854' :
-                                 '#ffd92f';
+    return d == "Jujuy"        ? '#e6ab02' :
+           d == "Santa Fe"     ? '#66a61e' :
+           d == "Misiones"     ? '#e7298a' :
+           d == "Tucuman"      ? '#7570b3' :
+           d == "Salta"        ? '#d95f02' :
+                                 '#1b9e77';
 }
 
 function defaultStyle(feature) {
@@ -104,37 +144,33 @@ var provincesLayer = new L.GeoJSON.AJAX("./data/provincias.geojson", {
 info.onAdd = function ( map ) {
     // create a div with a class "info"
     this._div = L.DomUtil.create( 'div', 'container info' )
-    this.setBlank()
+    this._div.innerHTML = box_base
     return this._div
-}
-
-info.setBlank = function () {
-    this._div.innerHTML = innerHTML_empty
 }
 
 info.update = function ( e ) {
     // Method that we will use to update the control based on feature properties passed
     var properties = e.target.feature.properties
 
-    //info._div.innerHTML = innerHTML_fill
-
     if ( properties.isEnabled ) {
-      // remove
       div = document.getElementsByClassName('info-text')[0]
-      div.innerHTML = ''
+      div.classList.remove('in')
+      div.innerHTML = properties.html_texto
+      div.className += ' in'
 
       // add the imgs
       div = document.getElementById('box-imgs')
-      div.innerHTML = imgs_html
+      div.innerHTML = properties.html_media
 
-      // plot the bar
-      plot(data[properties.name])
 
       // add the efect
-      box = document.getElementsByClassName('box')[0]
-      box.className += ' in'
+      //box = document.getElementsByClassName('box')[0]
+      //box.className += ' in'
 
-      lightGallery(document.getElementById('lightgallery'));
+      lightGallery(document.getElementById('lightgallery'))
+      lightGallery(document.getElementById('video-gallery'))
+
+
     }
 }
 
